43f25d485196e226c7ddb76f09b3c7114a67b9fb
Suryakant Yengudle <30753712+Sryengudle@users.noreply.github.com>
2020-09-27 11:53:12 +0530

    update app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,18 +2,11 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import FormBuilderContainer from "./FormBuilderContainer/FormBuilderContainer";
-import Button from "../components/GenericComponents/Button";
 import { handleDropdownOption, handleAddComponent, handleChangeRadio, handleAddRadio, handleRemoveComponent, handleChangeInput, downloadJson } from './Action';
-
-const buttonStyle = {
-  margin: "10px 10px 10px 10px"
-};
 
 class App extends Component {
 
-  // componentDidMount = () => this.props.downloadJson();
   handleRemoveComponent = idx => this.props.handleRemoveComponent(idx);
-
 
   handleChangeInput = (evt, idx) => {
     let name = evt.target.name;
@@ -29,13 +22,11 @@ class App extends Component {
 
   handleChangeRadio = (e, index, idx) => {
     let value = e.target.value;
-    console.log('handleChangeRadio',  index, idx)
     this.props.handleChangeRadio({ value, index, idx });
   }
 
 
   render() {
-
     return (
       <div className="col-md-6">
         <h3>Form Builder</h3>
@@ -45,21 +36,13 @@ class App extends Component {
           handleRemoveComponent={this.handleRemoveComponent}
           handleChangeInput={this.handleChangeInput}
           handleChangeRadio={this.handleChangeRadio}
+          downloadJson = {this.props.downloadJson}
           handleAddRadio={this.props.handleAddRadio}
         />
       </div>
     );
   }
 }
-
-
-// const mapStateToProps = state => {
-//   return {
-//     formDetails: state.formReducer.formDetails,
-//     componentOptions: state.formReducer.componentOptions,
-//     radioOption: state.formReducer.radioOption,
-//   }
-// }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   //Bind all the Actions which you want to dispatch in the container. 
